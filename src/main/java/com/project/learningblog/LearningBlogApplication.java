71c82bce7bd52744133ffdea4d56d2a9b5c4be84
changha <lchangha0812@gmail.com>
2024-12-23 10:40:47 +0900

    Initialize repository

---
+++ src/main/java/com/project/learningblog/LearningBlogApplication.java
@@ -0,0 +1,13 @@
+package com.project.learningblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/learningblog/LearningBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.learningblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
